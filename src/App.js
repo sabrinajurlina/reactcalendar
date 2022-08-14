8cc084d89a2c72dc0af561020b3f195851c7a4fb
Sabrina Jurlina <sabrina.jurlina@gmail.com>
2022-08-13 19:56:50 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+import Calendar from 'react-calendar'
+// import 'react-calendar/dist/Calendar.css';
+import React, {useState} from 'react';
+import moment from 'moment';
 
-function App() {
+
+export default function App() {
+  const [dateState, setDateState] = useState(new Date());
+  
+  //Change Date Method
+  const changeDate = (e) => {
+    setDateState(e)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <>
+    <div className='app'>
+      <h1 className='text-center'>React Calendar with Range</h1>
+      <div className='calendar-container'>
+          <Calendar
+            value = {dateState}
+            nextLabel='month >'
+            nextAriaLabel='Go to next month'
+            next2Label='year >>'
+            next2AriaLabel='Go to next year'
+            prevLabel='< month'
+            prevAriaLabel='Go to previous month'
+            prev2Label='<< year'
+            prev2AriaLabel='Go to previous year'
+            selectRange={true}
+            onChange={changeDate}
+          />
+      </div>
+      <>
+      {dateState.length > 0 ? (
+        <p className='text-center'>
+          <span className='bold'>Start:</span>{' '}
+            {dateState[0].toDateString()}
+            &nbsp;|&nbsp;
+          <span className='bold'>End:</span>{' '}
+            {dateState[1].toDateString()}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      ) : (
+        <p className='text-center'>
+          <span className='bold'>Default selected date:</span>{' '}
+          {dateState.toDateString()}
+        </p>
+      )}
+      </>
+      </div>
+    </>
   );
 }
-
-export default App;
